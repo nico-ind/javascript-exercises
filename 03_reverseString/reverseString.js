64763fe04fe645e2611d8f0aeecc8045a9f8d989
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-13 16:14:40 -0300

    solve ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversed = [];
+    
+    for (const letter of word) {
+        reversed.unshift(letter);
+    }
 
+    return reversed.join('')
 };
 
 // Do not edit below this line

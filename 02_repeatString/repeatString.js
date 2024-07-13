24ea74420cd662cf5f6d6c086b8a27236c5aa34b
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-13 16:05:30 -0300

    solve ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let repeatedString = '';
+    if (number >= 0){
+        for (i=0; i<number; i++) {
+            repeatedString = repeatedString.concat(string);
+        }
+    } else {
+        repeatedString = 'ERROR';
+    }   
 
+    return (repeatedString);
 };
 
 // Do not edit below this line

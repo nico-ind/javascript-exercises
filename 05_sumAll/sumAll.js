e8d6ddf415ea0e6f901a5fa77e1e7ca9d1ad9a5c
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-14 15:51:47 -0300

    solve ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    let numbers = [num1, num2];
+
+    let finalSum = 0;
+
+    // Ordenar de menor a mayor 
+    numbers.sort(function(a, b){return a-b});
+
+    // // Verificar que el input sea del tipo entero positivo
+    if (typeof (num1) !== 'number' || typeof (num2) !== 'number') {
+        return 'ERROR';
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else {
+        for (let i=numbers[0]; i<numbers[1]+1; i++) {
+            finalSum += i; 
+        }
+    }
+
+    return finalSum
 
 };
 

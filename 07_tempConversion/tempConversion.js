e18448e105ffc067cb3c0fa75e2e966e107b2ecd
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-14 16:47:25 -0300

    solve ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+ const celsius = (input - 32) * 5/9;
+  return Math.round(celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  const faren = (9/5) * input + 32;
+  return Math.round(faren * 10) / 10
 };
 
 // Do not edit below this line

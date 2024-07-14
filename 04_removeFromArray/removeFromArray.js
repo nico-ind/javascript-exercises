1dc3df762a628b2c80e348d00b39cf3d6bedd327
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-14 12:13:48 -0300

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...removedNumbers) {
+    
+    let outputArray = [];
+
+    for (const inputArrayELement of inputArray) {
+        
+        if (removedNumbers.includes(inputArrayELement)) {
+            continue
+            
+        } else {
+            outputArray.push(inputArrayELement);
+        }
+        
+    }
+
+    return outputArray
 };
 
 // Do not edit below this line

8712de066608bf986fb219db32e378c1f2e2f698
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-23 15:58:07 -0300

    solve ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+// function to add yearOfDeath to persons missing it
+function correct(person) {
 
+    const currentYear = (new Date()).getFullYear();
+
+    if (person.yearOfDeath === undefined) {
+        person['yearOfDeath'] = currentYear;
+    }
+
+    return person;
+}
+
+const findTheOldest = function(people) {
+
+    correctedPeople = people.map(correct)
+
+    oldest = correctedPeople.sort(function(a, b) {
+
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    
+    return oldest[0];
 };
 
 // Do not edit below this line

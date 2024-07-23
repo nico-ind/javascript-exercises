15bea538486e6f67fbf4459c022b8ba80dad5649
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-23 13:59:53 -0300

    solve ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (frase) {
+
+   // Split characters from frase and test with regex if it has a letter. If true, filter returns that character
+   onlyLetters = frase.toLowerCase().split('').filter(char => /[a-zA-Z0-9]/.test(char));
+
+   // check if it is a palindrome
+   let isPalindrome = onlyLetters.join('') === onlyLetters.reverse().join('') ? true : false;
+
+   return isPalindrome
 
 };
 

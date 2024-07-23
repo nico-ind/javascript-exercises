6621608e3b6f7a30cd0a0c40b67af71b8d082e6c
nico-ind <167833824+nico-ind@users.noreply.github.com>
2024-07-23 14:35:04 -0300

    solve ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) {
+        return "OOPS";
+
+    } else {
+        if (num === 0) {
+            return 0;
+
+        } else if (num === 1) {
+            return 1;
+
+        } else {
+            let fibonacci = [0 , 1];
+
+            for (let i = 2; i < num + 1; i++) {
+
+                const num_i = fibonacci[i-1] + fibonacci[i-2];
+                fibonacci.push(num_i);
+            }
+
+            return fibonacci[num];
+        }     
+    }
 };
 
 // Do not edit below this line
